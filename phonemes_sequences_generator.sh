19d66865060fe36d047bf596d415104868fa4acf
oserikov <srkvoa@gmail.com>
2018-11-23 04:13:36 +0300

    sorted alphabets via commandline sort

--- phonemes_sequences_generator.sh
+++ phonemes_sequences_generator.sh
@@ -1,2 +1,4 @@
-python3 py_scripts/phonemes_sequences_generator.py
+# given the line-by-line list of lexemes extracts all the substrings of length 2 from each lexeme
+# and line-by-line prints these substrings to stdout
 
+python3 py_scripts/phonemes_sequences_generator.py
