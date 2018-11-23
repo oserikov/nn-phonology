1dae41aadb6e93b319d038c410d23a2ae0d76f26
oserikov <srkvoa@gmail.com>
2018-11-23 14:22:54 +0300

    python encoding when reading input fix, typo fix

--- ml_input_data_generator.sh
+++ ml_input_data_generator.sh
@@ -1,3 +1,4 @@
- sh_scripts/lexemes_generator.sh |\
- sh_scripts/phonemes_sequences_extractor.sh |\
- sh_scripts/turkish_onehot_encoder.sh
+export PYTHONIOENCODING="UTF-8"
+sh_scripts/lexemes_generator.sh |\
+sh_scripts/phonemes_sequences_extractor.sh |\
+sh_scripts/turkish_onehot_encoder.sh
